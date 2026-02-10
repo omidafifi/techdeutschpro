273ec021657e6078bd2f3f7911c4cdabc975ebc9
omid <111409640+omidafifi@users.noreply.github.com>
2026-02-10 17:14:34 +0330

    desc Get goals //route Get /api/goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,5 +1,8 @@
+// desc Get goals
+//route Get /api/goals
+//
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals" });
 };
 
-module.exports = { getGoals };
+module.exports = {getGoals};
