06e780b67f17fe287a137dfdd7fea45a1cf61986
omid <111409640+omidafifi@users.noreply.github.com>
2026-02-12 10:35:46 +0330

    Error & Exception Handling

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,28 +1,30 @@
 // desc Get goals
 //route Get /api/goals
-//access
+//access private
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals" });
 };
 
 // desc set goal
 //route Post /api/goals
-//access
+//access private
 const setGoals = (req, res) => {
-  console.log(req.body);
+  if (!req.body) {
+    res.status(400).json({ message: "please add a text field" });
+  }
   res.status(200).json({ message: "Set goal" });
 };
 
 // desc Update goals
 //route Put /api/goals/:id
-//access 
+//access private
 const updateGoals = (req, res) => {
   res.status(200).json({ message: "Update goals" });
 };
 
 // desc Delete goals
 //route Delete /api/goals/:id
-//access
+//access private
 const deleteGoals = (req, res) => {
   res.status(200).json({ message: "Delete goals" });
 };
